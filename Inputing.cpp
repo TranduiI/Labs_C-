be5cda79588c9ada663c33fe9f4a00dc6ee05bd2
TranduiI <leonovich48@outlook.com>
2024-11-25 09:28:21 +0300

    1.0

---
+++ Inputing.cpp
@@ -0,0 +1,29 @@
+﻿// Inputing.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct YuliaException : public exception {
+    const char* what() const throw () {
+        return "C++ Exception";
+    }
+};
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    try
+    {
+        throw YuliaException();
+        cout << "shit"<<endl;
+    }
+    catch (YuliaException& e)
+    {
+        cout << "пиймав исключеню"<<endl;
+    }
+
+    cout << "пака"<<endl;
+        
+ }
