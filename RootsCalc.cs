7e527d0d54da2b42c8cd2c3cd13de51bfcf8d0c1
TranduiI <leonovich48@outlook.com>
2024-11-25 09:51:47 +0300

    1.0

---
+++ RootsCalc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba16
+{
+    public class RootsCalc
+    {
+        public static KeyValuePair<double, double> GetRoots(double a, double b, double c)
+        {
+            double D = b * b - 4 * a * c;
+            if (D >= 0)
+            {
+                double x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(D)) / (2 * a);
+                return new KeyValuePair<double, double>(x1, x2);
+            }
+            else
+            {
+                return default;
+            }
+        }
+    }
+}
---
+++ SqrRootsMultiValueConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Laba16
+{
+    class SqrRootsMultiValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (double.TryParse((string)values[0], out double a) &&
+                double.TryParse((string)values[1], out double b) &&
+                double.TryParse((string)values[2], out double c))
+            {
+                KeyValuePair<double, double> result = RootsCalc.GetRoots(a, b, c);
+                if (result.Equals(default(KeyValuePair<double, double>)) == true)
+                {
+                    return "Нет корней";
+                }
+                return $"X1: {result.Key}\nX2: {result.Value}";
+            }
+            else
+            {
+                return "Не является числом";
+            }
+
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return new object[3] { 0, 0, 0 };
+        }
+    }
+}
