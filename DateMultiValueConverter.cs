57b76163acc482c56e6855e6e33d840a725590da
TranduiI <leonovich48@outlook.com>
2024-11-25 09:41:17 +0300

    1.0

---
+++ DateMultiValueConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Laba13_A
+{
+    class DateMultiValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            string dateFormat = "d-M-yyyy";
+            
+            string parsedDate = (string)values[0]+"-"+(string)values[1]+"-"+(string)values[2];
+            DateTime dateTime;
+            if(DateTime.TryParseExact(parsedDate, dateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out dateTime))
+            {
+                return dateTime.ToString("dd.MM.yyyy");
+            }
+            else
+            {
+                return "Некорректная дата";
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            DateTime date;
+            string dateFormat = "dd.MM.yyyy";
+            if (DateTime.TryParseExact((string)value,dateFormat, CultureInfo.InvariantCulture,DateTimeStyles.None, out date))
+            {
+                string day = date.Day.ToString();
+                string month = date.Month.ToString();
+                string year = date.Year.ToString();
+                return new object[] { day, month, year };
+            }
+            else
+            {
+                return new object[] { "", "", "" };
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laba13_A
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private bool IsNumber(string enteredText)
+        {
+            if (!enteredText.All(c => Char.IsNumber(c)))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void TextBoxNumberOnly(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsNumber(e.Text);
+            base.OnPreviewTextInput(e);
+        }
+    }
+}
