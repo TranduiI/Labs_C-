2e6d6365e47c2357e895b563a7782e4d4a9f81d9
TranduiI <leonovich48@outlook.com>
2024-11-25 09:35:22 +0300

    1.0

---
+++ Laba1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿using System;
+
+namespace Laba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите b";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 98);
+            this.textBox1.MaxLength = 12;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите a";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(140, 98);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(339, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 27);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(257, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Введите острый угол";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.IndianRed;
+            this.button2.Location = new System.Drawing.Point(364, 122);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(58, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Закрыть";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(260, 98);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(75, 169);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(74, 17);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Трапеция";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(183, 169);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(114, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Параллелограмм";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(434, 236);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Вычисление функции с помощью разложения в ряд";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ Laba1/Form1.cs
@@ -0,0 +1,395 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Windows.Forms;
+
+
+namespace Laba1
+{
+
+
+
+    public partial class Form1 : Form
+    {
+
+        private static DialogResult ShowThreadExceptionDialog(string title, Exception e) //Метод для выведения ошибок (исключений) в отдельном окне
+        {
+            string errorMsg = e.Message + "\n"; //Тело ошибки 
+            
+            return MessageBox.Show(errorMsg, title, MessageBoxButtons.OK, 
+                MessageBoxIcon.Stop); //Вывод окна ошибки / title - заголовок 
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void textBox1_TextChanged(object sender, EventArgs e) // Метод реагирующий на изменение содержания в textBox1
+        {
+
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                button1.Enabled = false;
+            }
+            else
+            {
+                button1.Enabled = true;
+            }
+
+
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e) // Метод реагирующий на нажатие клавиши в поле textBox1
+        {
+            if (char.IsDigit(e.KeyChar)) // Если клавиша является цифрой то
+            {
+                if (e.KeyChar == '0' && textBox1.Text.Length == 0) //Если в поле еще ничего не введено
+                {
+                    e.Handled = true; //то запрещаем вводить 0
+                }
+                else //если уже что-то есть
+                {
+                    return; //разрешаем
+
+                }
+            }
+            
+                
+            
+            char aDecimalSeparator = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator[0]; // Создаем переменную aDecimalSeparator которой присваем значение точки '.' 
+                if (e.KeyChar == '.' || e.KeyChar == ',') // Если кнопкой будет '.' или ',' то приравниваем ее к переменной aDecimalSeparator - '.'
+                {
+                    e.KeyChar = aDecimalSeparator;
+                }
+                if (e.KeyChar == aDecimalSeparator) //если кнопка равняется aDecimalSeparator - '.' то
+                {
+                    if (((TextBox)sender).Text.IndexOf(aDecimalSeparator) != -1) //если в поле textBox1 уже есть символ который подходит под aDecimalSeparator ('.' или ',') то
+                    {
+                        e.Handled = true; //его нельзя ввести
+                    }//
+                    
+                    return; // можно ввести если его нет
+                }
+                if (Char.IsControl(e.KeyChar))
+                {
+                    if (e.KeyChar == (char)Keys.Enter) textBox2.Focus();
+                    return;
+                }
+                e.Handled = true;
+                   
+        }
+
+
+        
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            {
+                if (textBox1.Text == "" || textBox2.Text == "")
+                {
+                    button1.Enabled = false;
+                }
+                else
+                {
+                    button1.Enabled = true;
+                }
+            }
+        }
+
+       
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar))
+            {
+                if (e.KeyChar == '0' && textBox2.Text.Length == 0) //
+                {
+                    e.Handled = true;
+                }
+                else
+                {
+                    return;
+
+                }
+            }
+            char aDecimalSeparator = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator[0]; // Создаем переменную aDecimalSeparator которой присваем значение точки '.' 
+            if (e.KeyChar == '.' || e.KeyChar == ',') // Если кнопкой будет '.' или ',' то приравниваем ее к переменной aDecimalSeparator - '.'
+            {
+                e.KeyChar = aDecimalSeparator;
+            }
+            if (e.KeyChar == aDecimalSeparator) //если кнопка равняется aDecimalSeparator - '.' то
+            {
+                if (((TextBox)sender).Text.IndexOf(aDecimalSeparator) != -1) //если в поле textBox2 уже есть символ который подходит под aDecimalSeparator ('.' или ',') то
+                {
+                    e.Handled = true; //нельзя ввести 
+                }//
+
+                return; // можно ввести
+            }
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Enter) textBox3.Focus();
+                return;
+            }
+            e.Handled = true;
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                button1.Enabled = false;
+            }
+            else
+            {
+                button1.Enabled = true;
+            }
+        }
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar))
+            {
+                if (e.KeyChar == '0' && textBox3.Text.Length == 0) //
+                {
+                    e.Handled = true;
+                }
+                else
+                {
+                    return;
+                    
+                }
+            }
+
+            
+            char aDecimalSeparator = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator[0]; // Создаем переменную aDecimalSeparator которой присваем значение точки '.' 
+            if (e.KeyChar == '.' || e.KeyChar == ',') // Если кнопкой будет '.' или ',' то приравниваем ее к переменной aDecimalSeparator - '.'
+            {
+                e.KeyChar = aDecimalSeparator;
+            }
+            if (e.KeyChar == aDecimalSeparator) //если кнопка равняется aDecimalSeparator - '.' то
+            {
+                if (((TextBox)sender).Text.IndexOf(aDecimalSeparator) != -1) //если в поле textBox1 уже есть символ который подходит под aDecimalSeparator ('.' или ',') то
+                {
+                    e.Handled = true; //его нельзя ввести
+                }//
+
+                return; // если в поле его нет, то его ввести можно
+            }
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Enter) textBox3.Focus();
+                return;
+            }
+            e.Handled = true;
+            //if(Convert.ToDouble(textBox3.Text) >= 180)        //Заготовка проверки на то, что в поле Угла - нельзя ввести значение больше 180 градусов #Не работает
+            //{
+            //    e.Handled = true;
+            //}
+            //else
+            //{
+            //    return;
+            //}
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+            if (radioButton.Checked)
+            {
+                MessageBox.Show("Вы выбрали " + radioButton.Text);
+                return;
+            };
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+            if (radioButton.Checked)
+            {
+                MessageBox.Show("Вы выбрали " + radioButton.Text);
+                return;
+            };
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+      
+                DialogResult dialog = MessageBox.Show(
+                 "Вы действительно хотите выйти из программы?",
+                 "Завершение программы",
+                 MessageBoxButtons.YesNo,
+                 MessageBoxIcon.Warning
+                );//вывод окна с предупреждением о закрытие формы
+                if (dialog == DialogResult.Yes)
+                {
+                    this.Close();
+                    //закрытие формы;
+                }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public class Figura
+        {
+            double a;
+            double b;
+            double angle;
+            double perim;
+            double sq;
+
+
+            public Figura(double A, double B, double ANGLE, bool rb1, bool rb2)
+            {
+                a = A;
+                b = B;
+                angle = Math.PI * ANGLE / 180.0;
+                if(rb1 == true && rb2 == false)
+                {
+                    double d = Math.Cos(angle) * A;
+                    double h = Math.Sin(angle) * A;
+                    double c = (2 * d) + B;
+                    perim = (2 * A) + B + c;
+                    sq = ((B + c) / 2) * h;
+
+                }
+                else if (rb1 == false && rb2 == true)
+                {
+                    perim = (2 * A) + (2 * B);
+
+                    sq = A * B * Math.Sin(angle);
+                }
+                else
+                {
+                    MessageBox.Show("Вы не выбрали ни один из вариантов");
+                    perim = 0;
+                    sq = 0;
+                }
+            }
+            public double A { get { return a; } }
+            public double B { get { return b; } }
+            public double ANGLE { get { return angle; } }
+            public double PERIMETR { get { return perim; } }
+            public double SQUARE { get { return sq; } }
+
+
+
+        }
+        //private Double PerSqTrap(Figura figura, out Double sq) /*Функция расчёта суммы ряда*/
+        //{
+           
+
+           
+        //   // double angle = Math.PI * alfa / 180.0;
+        //    double d = Math.Cos(figura.ANGLE) * figura.A;
+        //    double h = Math.Sin(figura.ANGLE) * figura.A;
+        //    double c = (2 * d) + figura.B;
+        //    double perim = (2* figura.A) +figura.B +c;
+            
+        //    sq = ((figura.B + c) / 2) * h;
+            
+        //    return perim;
+        //}
+
+
+        //private Double Square(Double a, Double b, Double alfa)
+        //{
+        //    double angle = Math.PI * alfa / 180.0;
+        //    double d = Math.Cos(angle) * a;
+        //    double h = Math.Sin(angle) * a;
+        //    double c = (2 * d) + b;
+
+        //    double square = ((b + c) / 2) * h;
+
+        //    return square;
+        //}
+
+        //private Double PerSqPar(Figura figura, out Double sq) /*Функция расчёта суммы ряда*/
+        //{ 
+        //    double perim = (2 * figura.A) + (2 * figura.B);
+
+        //    sq = figura.A * figura.B* Math.Sin(figura.ANGLE);
+
+        //    return perim;
+        //}
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Figura fig = new Figura(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox3.Text), radioButton1.Checked, radioButton2.Checked == true);
+
+
+            label3.Text = "Периметр: " + fig.PERIMETR + "\n" + "Площадь: " + fig.SQUARE;
+
+            // Задаем значения для передачи методу Taylor и создаем переменную для контрольной проверки 
+            //double perim, sq;
+            //if (radioButton1.Checked == true)
+            //{
+            //    perim = PerSqTrap(fig, out sq);
+            //    label3.Text = "Периметр: " + perim + "\n" + "Площадь: " + sq;
+            //}
+
+
+            //else if (radioButton2.Checked == true)
+            //{
+            //    perim = PerSqPar(fig, out sq);
+            //    label3.Text = "Периметр: " + perim + "\n" + "Площадь: " + sq;
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Вы не выбрали ни один из вариантов");
+            //}
+
+
+
+
+
+
+
+            //Альтернативный метод округления
+
+            //int c = -(int)math.log10(acc);
+            //string f = "g" + c.tostring();
+
+            //label3.text = string.format("ответ:\n" + "cos(x) = {1:" + f + "}", arg, cos) + string.format("\n сумма ряда = {0:" + f + "}", sum) + "\nколичество членов ряда = " + n;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
+
