d39cb998a18df643176b42f26ae89c0dfe867f17
TranduiI <leonovich48@outlook.com>
2024-11-25 09:17:46 +0300

    AutoDoc

---
+++ AutoDocumentary.cpp
@@ -0,0 +1,58 @@
+﻿// AutoDocumentary.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <iostream>
+using namespace std;
+
+
+
+/// <summary>
+/// Рабочее место
+/// </summary>
+class WorkingPlace {
+    private:
+    /// <summary>
+    /// Дни без проблем
+    /// </summary>
+    int days_without_problems = 0;
+
+    public:
+    /// <summary>
+    /// Устанавливает количество дней без проблем и выводит их на экран
+    /// </summary>
+    /// <param name="Дни без проблем"></param>
+    /// <permission cref="System::Security::PermissionSet">Everyone can access this method.</permission>
+    void SetDaysWithoutProblems(int days) {
+        days_without_problems = days;
+    }
+    /// <summary>
+    /// <c>ComeToHospital</c> используется, когда происходит проблема
+    /// </summary>
+    /// <param name="Дни без проблем"></param>
+    /// <returns>Значение дней без проблем становится равным нулю</returns>
+    int SomeNewProblem() {       
+        days_without_problems = 0;
+        return days_without_problems;
+    }
+    /// <summary>
+    /// Выводит на экран количество дней без проблем
+    /// </summary>
+    void PrintDaysWithoutProblems() {
+        cout << days_without_problems << endl;
+    }
+
+};
+
+int main()
+{
+    /// <summary>
+    /// Офис
+    /// </summary>
+    WorkingPlace office;
+    office.SetDaysWithoutProblems(5);
+    office.PrintDaysWithoutProblems();
+    office.SomeNewProblem();
+    office.PrintDaysWithoutProblems();
+}
+
---
+++ XMLDocs.xdc
@@ -0,0 +1,36 @@
+<?xml version="1.0"?><doc>
+<members>
+<member name="T:WorkingPlace" decl="false" source="C:\Users\TranduilPC\Desktop\C++\AutoDocumentary\AutoDocumentary.cpp" line="10">
+<summary>
+Рабочее место
+</summary>
+</member>
+<member name="F:WorkingPlace.days_without_problems" decl="false" source="C:\Users\TranduilPC\Desktop\C++\AutoDocumentary\AutoDocumentary.cpp" line="15">
+<summary>
+Дни без проблем
+</summary>
+</member>
+<member name="M:WorkingPlace.SetDaysWithoutProblems(System.Int32)" decl="false" source="C:\Users\TranduilPC\Desktop\C++\AutoDocumentary\AutoDocumentary.cpp" line="21">
+<summary>
+Устанавливает количество дней без проблем и выводит их на экран
+</summary>
+<param name="Дни без проблем"></param>
+<permission cref="!:System::Security::PermissionSet">Everyone can access this method.</permission>
+</member>
+<member name="M:WorkingPlace.SomeNewProblem" decl="false" source="C:\Users\TranduilPC\Desktop\C++\AutoDocumentary\AutoDocumentary.cpp" line="29">
+<summary>
+<c>ComeToHospital</c> используется, когда происходит проблема
+</summary>
+<param name="Дни без проблем"></param>
+<returns>Значение дней без проблем становится равным нулю</returns>
+</member>
+<member name="M:WorkingPlace.PrintDaysWithoutProblems" decl="false" source="C:\Users\TranduilPC\Desktop\C++\AutoDocumentary\AutoDocumentary.cpp" line="38">
+<summary>
+Выводит на экран количество дней без проблем
+</summary>
+<example>
+Дней без происшествий - 4
+</example>
+</member>
+</members>
+</doc>
