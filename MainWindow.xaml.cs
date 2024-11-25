97fc971fab116e7372699699b94b57b7b9fb6139
TranduiI <leonovich48@outlook.com>
2024-11-25 09:53:12 +0300

    1.0

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laba18_Client
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SendBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (client == null || !client.Connected)
+                {
+                    client = new TcpClient("127.0.0.1", 1111);
+                    stream = client.GetStream();
+                }
+
+                string message = MessageText.Text;
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                ServerResponseBox.Text += $"{DateTime.Now} (Server): " + response + Environment.NewLine;
+                MessageText.Clear();
+                
+            }
+
+            catch (Exception)
+            {
+                ServerResponseBox.Text += "Server is not responding! May be caused by stoped Server.\n";
+            }
+        }
+    }
+}
