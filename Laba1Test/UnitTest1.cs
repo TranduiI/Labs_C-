f99642bebf310573476a438600d4b6712e513552
TranduiI <leonovich48@outlook.com>
2024-11-25 09:36:02 +0300

    1.0

---
+++ Laba1Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Laba1Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
